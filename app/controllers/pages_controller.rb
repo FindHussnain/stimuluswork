59982a4077f3be654e001007149a94bbb6b950f2
Hussnain Younas <findhussnain@gmail.com>
2021-11-10 10:17:13 +0500

    Add description to a label on selection of a radio button using stimulus

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def iceCream
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'icecream', to: "pages#iceCream"
   resources :posts
   resources :tables, only: [:show, :create, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
